def7c7e9196b0b558d5533d0637d80bdc1040227
Shawal Ahmad Mohmand <criticalmiind@gmail.com>
2023-03-12 03:23:25 +0500

    like apis changes

--- model/comments.js
+++ model/comments.js
@@ -1,27 +1,28 @@
 const db = require("../config/database");
 
 module.exports = class Comments {
-  constructor() { }
+    constructor() { }
 
-  fetchFoodPostComments(foodId, userId) {
-    return db.execute(`
-    SELECT c.*, u.*,
-    (SELECT COUNT(*) FROM commentsliked cl WHERE cl.commentId = c.commentId) AS likes, 
-    CASE WHEN li.userId = '${userId}' THEN true ELSE false END AS isLiked
-    FROM comments c
-    JOIN users u ON c.userId = u.userId
-    WHERE c.foodId = ${foodId}`);
-  }
+    fetchFoodPostComments(foodId, userId) {
+        return db.execute(`
+            SELECT c.*, u.*,
+            (SELECT COUNT(*) FROM commentsliked cl WHERE cl.commentId = c.commentId) AS likes, 
+            CASE WHEN cl.userId = '${userId}' THEN true ELSE false END AS isLiked
+            FROM comments c
+            JOIN users u ON c.userId = u.userId
+            LEFT JOIN commentsliked cl ON cl.commentId = c.commentId
+            WHERE c.foodId = ${foodId}`);
+    }
 
-  postComment(userId, foodId, comment) {
-    return db.execute(`INSERT INTO comments SET userId = ${userId}, foodId = ${foodId}, comment = '${comment}', status = 1`);
-  }
+    postComment(userId, foodId, comment) {
+        return db.execute(`INSERT INTO comments SET userId = ${userId}, foodId = ${foodId}, comment = '${comment}', status = 1`);
+    }
 
-  updateComment(commentId, comment) {
-    return db.execute(`UPDATE comments SET comment = '${comment}' WHERE commentId=${commentId}`);
-  }
+    updateComment(commentId, comment) {
+        return db.execute(`UPDATE comments SET comment = '${comment}' WHERE commentId=${commentId}`);
+    }
 
-  deleteComment(commentId) {
-    return db.execute(`DELETE FROM comments WHERE commentId = ${commentId}`);
-  }
+    deleteComment(commentId) {
+        return db.execute(`DELETE FROM comments WHERE commentId = ${commentId}`);
+    }
 };
